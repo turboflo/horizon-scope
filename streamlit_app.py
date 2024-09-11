82bbb41e788146f271b7e9cdaa41b8cfdd6f7056
Florian Hegenbarth <florian@hegenbarth.dev>
2024-09-11 14:09:18 +0200

    chore: Add src directory to Python path in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the src directory to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
 from horizon_scope.presentation.streamlit_presentation import main
 
 if __name__ == "__main__":
